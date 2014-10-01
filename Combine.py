84baf905cb1e4c3aa14d39d88d5c97cf6190c71c
Geoff Russell <Geoff.R.Russell@gmail.com>
2014-10-01 10:25:26 -0700

    Used try, except block to check for values that should be floats to convert text in a more robust way

--- Combine.py
+++ Combine.py
@@ -24,11 +24,11 @@ for csvfile in sortedFiles:
         reader = csv.reader(f)
         for r, row in enumerate(reader):
             for c, val in enumerate(row):
-                if r > 4:
-                    ws.write(r,c,float(val)) #Cconverts all data to floats
-                else:
+                try:
+                    ws.write(r,c,float(val)) #Converts all numbers to floats
+                except ValueError:
                     ws.write(r, c, val)
     print 'Added %s' %csvfile
 
-wb.save(filename + '.xlsx')
+wb.save(filename + '.xls')
 print 'Wrote ' + filename
