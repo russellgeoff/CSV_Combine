a4c65c55d43a9ba7d32d6ffe30e6d61189ff8dc7
Geoff Russell <Geoff.R.Russell@gmail.com>
2014-11-13 11:24:05 -0800

    Summarizes data on the first page of the document Added PyCharm project files

--- Combine.py
+++ Combine.py
@@ -16,43 +16,50 @@ while filename == "":
 
 #Sets up workbook & adds data summary sheet
 wb = openpyxl.Workbook()
-ws = wb.active
-ws.title = 'Data Summary'
+ws_summary = wb.active
+ws_summary.title = 'Data Summary'
+ws_summary.cell(row=1, column=1).value = 'Sample'
+ws_summary.cell(row=1, column=2).value = 'Max Load'
+
+summary_row = 2
 
 #Gets list of all CSV files in the current directory and sorts them intelligently
 files = glob.glob(os.path.join('.', '*.csv'))
 sortedFiles = natsort.natsorted(files)
 
 #Puts each CSV file into a new sheet
-for csvfile in sortedFiles:
-    head, tail = os.path.split(csvfile)
+for csv_file in sortedFiles:
+    head, tail = os.path.split(csv_file)
     ws = wb.create_sheet()
     ws.title = tail
-    with open(csvfile, 'rb') as f:
+    with open(csv_file, 'rb') as f:
         reader = csv.reader(f)
         for r, row in enumerate(reader):
             for c, val in enumerate(row):
-                cell = ws.cell(row = r+1, column = c+1)
+                cell = ws.cell(row=r+1, column=c+1)
                 try:
-                    cell.value = float(val) #Trys to convert all numbers to floats
+                    cell.value = float(val)  # Tries to convert all numbers to floats
                 except ValueError:
                     try:
-                        cell.value = str(val) #Trys to submit the string value
+                        cell.value = str(val)  # Tries to submit the string value
                     except UnicodeDecodeError:
                         try:
-                            print("Error decoding %s in cell (row = %i, column = %i)\n" \
-                                  "Trying a different encoding standard" \
-                                  %(val, r+1, c+1))
+                            print("Error decoding %s in cell (row = %i, column = %i)\n"
+                                  "Trying a different encoding standard (Unicode ISO-8859-1)"
+                                  % (val, r+1, c+1))
                             cell.value = unicode(val, "ISO-8859-1")
                             print("Different encoding standard worked!")
                         except:
                             print("Decoding failed\nThis cell will be blank")
                 except:
-                    print("Other Error\nError occured in cell "\
-                          "(row = %i, column = %i). The value was %s"\
-                          %(r,c,val))
+                    print("Other Error\nError occured in cell "
+                          "(row = %i, column = %i). The value was %s"
+                          % (r, c, val))
                     print val
-    print('Added %s' %tail)
+    ws_summary.cell(row=summary_row, column=1).value = ws.title
+    ws_summary.cell(row=summary_row, column=2).value = '=MAX(%s!C:C)' % ws.title
+    summary_row +=1
+    print('Added %s' % tail)
 
 wb.save(filename + '.xlsx')
 print('Wrote ' + filename)
