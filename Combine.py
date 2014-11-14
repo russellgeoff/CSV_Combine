dcac423abd46c9c66bce813fdb8606c4c31b07fd
Geoff Russell <Geoff.R.Russell@gmail.com>
2014-11-13 17:10:25 -0800

    Added generic summary dictionary

--- Combine.py
+++ Combine.py
@@ -9,19 +9,32 @@ import csv
 import openpyxl
 import natsort
 
+__author__ = 'Geoff Russell'
+__version__ = '1.0'
+
+print("CSV Combine version: %s \n"
+      "Created by %s \n\n" % (__version__, __author__))
 filename = raw_input("What do you want the merged file to be called? ")
 while filename == "":
     print("ERROR: Filename cannot be blank!")
     filename = raw_input("What do you want the merged file to be called? ")
 
 #Sets up workbook & adds data summary sheet
+summary_row = 1
+summary_ws_data = {
+    'title': 'Data Summary',
+    'col1_title': 'Sample',
+    'col1_formula': '%s',
+    'col2_title': 'Removal',
+    'col2_formula': "=MAX('%s'!C:C)"
+}
+
 wb = openpyxl.Workbook()
 ws_summary = wb.active
-ws_summary.title = 'Data Summary'
-ws_summary.cell(row=1, column=1).value = 'Sample'
-ws_summary.cell(row=1, column=2).value = 'Max Load'
-
-summary_row = 2
+ws_summary.title = summary_ws_data['title']
+ws_summary.cell(row=summary_row, column=1).value = summary_ws_data['col1_title']
+ws_summary.cell(row=summary_row, column=2).value = summary_ws_data['col2_title']
+# ws_summary.cell(row=summary_row, column=3).value = 'Removal'
 
 #Gets list of all CSV files in the current directory and sorts them intelligently
 files = glob.glob(os.path.join('.', '*.csv'))
@@ -56,9 +69,15 @@ for csv_file in sortedFiles:
                           "(row = %i, column = %i). The value was %s"
                           % (r, c, val))
                     print val
-    ws_summary.cell(row=summary_row, column=1).value = ws.title
-    ws_summary.cell(row=summary_row, column=2).value = '=MAX(%s!C:C)' % ws.title
-    summary_row +=1
+    # Summary Worksheet Information
+    summary_row += 1
+    # ws_summary.cell(row=summary_row, column=1).value = ws.title
+    ws_summary.cell(row=summary_row, column=1).value = summary_ws_data['col1_formula'] % ws.title
+
+    ws_summary.cell(row=summary_row, column=2).value = summary_ws_data['col2_formula'] % ws.title
+
+    # ws_summary.cell(row=summary_row, column=3).value = '=MAX(%s!C:C)' % ws.title
+    # ws_summary.cell(row=summary_row, column=3).value = '=MAX(%s!C:C))' % ws.title
     print('Added %s' % tail)
 
 wb.save(filename + '.xlsx')
